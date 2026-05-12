920ad7d0aa16a6c46dd39707fe524e85ae61e47c
malikmajid161 <malikmajid1063j@gmail.com>
2026-05-12 10:08:42 +0000

    Deploying to gh-pages from @ malikmajid161/library-management@d6c4ffefe6b6586b67cf92f8124fac312660b5be 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2669394328" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1367104776" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
